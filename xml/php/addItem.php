d71924b097b4c92493af5e73a400d68cb2e05126
Maciej Fanok <62944583+llamam@users.noreply.github.com>
2020-05-27 19:58:51 +0200

    Update addItem.php

--- xml/php/addItem.php
+++ xml/php/addItem.php
@@ -12,82 +12,46 @@
     <?php
         if(isset($_REQUEST['ok'])) {
             $xml = new DOMDocument("1.0", "UTF-8");
-            $xml -> load("questions/questions.xml");
+            $xml -> load("../items.xml");
 
-            $correctAnswer = $_POST['answers'];
+            $rootTag = $xml->getElementsByTagName("items")-> item(0);
 
-            $rootTag = $xml->getElementsByTagName("QUESTIONS")-> item(0);
+            $dataTag = $xml->createElement("item");
 
-            $dataTag = $xml->createElement("QUESTION");
-            $correctAttribute = $xml->createAttribute('correct');
-            $correctAttribute->value = 'yes';
+            $nameTag = $xml->createElement("name",$_REQUEST['NAME']);
+            $despriceTag = $xml->createElement("description",$_REQUEST['DESCRIPTION']);
+            $purchaseTag = $xml->createElement("dop",$_REQUEST['DOP']);
+            $stateTag = $xml->createElement("state",$_REQUEST['STATE']);
+            $priceTag = $xml->createElement("price",$_REQUEST['PRICE']);
 
-            $categoryTag = $xml->createElement("CATEGORY",$_REQUEST['CATEGORY']);
-            $textTag = $xml->createElement("TEXT",$_REQUEST['TEXT']);
-            $aTag = $xml->createElement("A",$_REQUEST['A']);
-            $bTag = $xml->createElement("B",$_REQUEST['B']);
-            $cTag = $xml->createElement("C",$_REQUEST['C']);
-            $dTag = $xml->createElement("D",$_REQUEST['D']);
-
-            if ($correctAnswer == 'A') {
-                $aTag->appendChild($correctAttribute);
-
-            } else if ($correctAnswer == 'B') {
-                $bTag->appendChild($correctAttribute);
-
-            } else if ($correctAnswer == 'C') {
-                $cTag->appendChild($correctAttribute);
-
-            } elseif ($correctAnswer == 'D') {
-                $dTag->appendChild($correctAttribute);
-
-            }
-
-            $dataTag->appendChild($categoryTag);
-            $dataTag->appendChild($textTag);
-            $dataTag->appendChild($aTag);
-            $dataTag->appendChild($bTag);
-            $dataTag->appendChild($cTag);
-            $dataTag->appendChild($dTag);
+            $dataTag->appendChild($nameTag);
+            $dataTag->appendChild($despriceTag);
+            $dataTag->appendChild($purchaseTag);
+            $dataTag->appendChild($stateTag);
+            $dataTag->appendChild($priceTag);
 
             $rootTag -> appendChild($dataTag);
 
-            $xml -> save("questions/questions.xml");
+            $xml -> save("../items.xml");
     }
 
     ?>
 
     <div id="nav">
-        <h2 class="title"><a href="index.html">Baza pytań</a></h2>
+        <h2 class="title"><a href="../index.html">Baza sprzętu</a></h2>
     </div>
 
     <div class="container">
 
-        <div id="dodajpytanie">
-            <form action="addquestion.php" method="post">
-                <input name="CATEGORY" class="inputShort" style="width: 150px;" type="text" placeholder="Kategoria" >
-                <div id="break"></div>
-                <input name="TEXT" class="inputShort" style="width: 95%;" type="text" placeholder="Pytanie">
-                <div id="break"></div>
-                <input name="A" class="inputShort" type="text" placeholder="Odpowiedź A" >
-                <input name="B" class="inputShort" type="text" placeholder="Odpowiedź B" >
-                <div id="break"></div>
-                <input name="C" class="inputShort" type="text" placeholder="Odpowiedź C" >
-                <input name="D" class="inputShort" type="text" placeholder="Odpowiedź D" >
-                <div id="break"></div>
-                <label for="answers">Poprawna odpowiedź:</label>
-                <select name="answers" id="cars">
-                    <option value="A">A</option>
-                    <option value="B">B</option>
-                    <option value="C">C</option>
-                    <option value="D">D</option>
-                </select>
-                <input name="ok" class="buttonStyle" style="width: 150px;" type="submit" value="Dodaj pytanie">
+        <div id="addItem">
+            <form action="addItem.php" method="post">
+                <input name="NAME" type="text" placeholder="Nazwa sprzętu" >
+                <input name="DESCRIPTION" style="height: 200px;" type="text" placeholder="Opis">
+                <input name="DOP" class="inputShort" type="text" placeholder="Data zakupu" >
+                <input name="STATE" class="inputShort" type="text" placeholder="Stan" >
+                <input name="PRICE" class="inputShort" type="text" placeholder="Cena" >
+                <input name="ok" type="submit" value="Dodaj pytanie">
             </form>
-        </div>
-
-        <div id="">
-
         </div>
     </div>
 
