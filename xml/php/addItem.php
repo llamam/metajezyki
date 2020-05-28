6e405bde699b20e4813bc534322d0c09a4d0f51d
Maciej Fanok <62944583+llamam@users.noreply.github.com>
2020-05-28 19:52:03 +0200

    STYL v2

--- xml/php/addItem.php
+++ xml/php/addItem.php
@@ -5,6 +5,9 @@
     <title>Baza pytań</title>
     <meta charset="UTF-8">
     <link rel="stylesheet" href="styles/style.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
+        integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous" />
+
 </head>
 
 <body>
@@ -46,13 +49,15 @@
     <div class="container">
 
         <div id="addItem">
+        <h2> Formularz dodania produktu</h2>
+            <div id="break"></div>
             <form action="addItem.php" method="post">
-                <input name="NAME" type="text" placeholder="Nazwa sprzętu" >
-                <input name="DESCRIPTION" style="height: 200px;" type="text" placeholder="Opis">
-                <input name="DOP" class="inputShort" type="text" placeholder="Data zakupu" >
-                <input name="STATE" class="inputShort" type="text" placeholder="Stan" >
-                <input name="PRICE" class="inputShort" type="text" placeholder="Cena" >
-                <input name="ok" type="submit" value="Dodaj rekord">
+                <input class="form-control" name="NAME" type="text" placeholder="Nazwa sprzętu" >
+                <input class="form-control" name="DESCRIPTION" type="text" placeholder="Opis">
+                <input class="form-control" name="DOP"  type="text" placeholder="Data zakupu" >
+                <input class="form-control" name="STATE" type="text" placeholder="Stan" >
+                <input class="form-control" name="PRICE" type="text" placeholder="Cena" >
+                <input class="form-control" name="ok" type="submit" value="Dodaj rekord">
             </form>
         </div>
     </div>
