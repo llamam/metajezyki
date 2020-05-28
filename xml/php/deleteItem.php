1ad807678306541b43c1ce9ba48dbf0263f7cbc1
Maciej Fanok <62944583+llamam@users.noreply.github.com>
2020-05-28 20:23:22 +0200

    strona główna, styl, formularze

--- xml/php/deleteItem.php
+++ xml/php/deleteItem.php
@@ -4,7 +4,7 @@
 <head>
     <title>Baza pytań</title>
     <meta charset="UTF-8">
-    <link rel="stylesheet" href="styles/style.css">
+    <link rel="stylesheet" href="../styles/style.css">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
         integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous" />
 </head>
@@ -32,17 +32,28 @@
         }
     ?>
 
-    <div id="nav">
-        <h2 class="title"><a href="../index.html">Baza sprzętu</a></h2>
-    </div>
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <a class="navbar-brand" href="../index.html">Baza przedmiotów</a>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item">
+                    <a class="nav-link" href="../items.xml">Przedmioty</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="../stock.xml">Stan magazynu</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
 
     <div class="container">
 
-        <div id="deleteItem">
+        <div id="container">
             <h2> Formularz usuwania produktu</h2>
             <div id="break"></div>
             <form action="deleteItem.php" method="post">
                 <input class="form-control" name="NAME" type="text" placeholder="Nazwa sprzętu" >
+                <div id="break"></div>
                 <input class="btn btn-primary" name="ok" type="submit" value="Usuń">
             </form>
         </div>
